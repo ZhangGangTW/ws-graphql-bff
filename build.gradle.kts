b05346c00b410c035aef4f246df455b9e462d33d
Zhang Gang <gzhang@thoughtworks.com>
2021-03-29 17:06:53 +0800

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
 	id("org.springframework.boot") version "2.4.4"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.4.31"
 	kotlin("plugin.spring") version "1.4.31"
-	id("org.springframework.experimental.aot") version "0.9.1"
 }
 
 group = "com.workshop"
@@ -20,7 +18,6 @@ configurations {
 }
 
 repositories {
-	maven { url = uri("https://repo.spring.io/release") }
 	mavenCentral()
 }
 
@@ -46,9 +43,4 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
-}
-
-tasks.withType<BootBuildImage> {
-	builder = "paketobuildpacks/builder:tiny"
-	environment = mapOf("BP_NATIVE_IMAGE" to "true")
 }
