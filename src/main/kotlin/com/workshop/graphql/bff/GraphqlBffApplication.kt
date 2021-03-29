67a0c69718ff4641f6df4c639e5bc1acec147d45
Zhang Gang <gzhang@thoughtworks.com>
2021-03-29 15:09:12 +0800

    first commit

---
+++ src/main/kotlin/com/workshop/graphql/bff/GraphqlBffApplication.kt
@@ -0,0 +1,11 @@
+package com.workshop.graphql.bff
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphqlBffApplication
+
+fun main(args: Array<String>) {
+	runApplication<GraphqlBffApplication>(*args)
+}
---
+++ src/test/kotlin/com/workshop/graphql/bff/GraphqlBffApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.workshop.graphql.bff
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphqlBffApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
