aba40fe2f1629f6c413bf960f93ff7f74d7f51cc
Zhang Gang <gzhang@thoughtworks.com>
2021-04-01 14:37:00 +0800

    refactor
    (cherry picked from commit 6a05a5fe735bbf72838992ac63e99de23038d77c)

--- src/main/kotlin/com/workshop/graphql/bff/client/dto/UserDto.kt
+++ src/main/kotlin/com/workshop/graphql/bff/client/dto/UserDto.kt
@@ -1,9 +1,23 @@
 package com.workshop.graphql.bff.client.dto
 
+import com.workshop.graphql.bff.type.Group
+import com.workshop.graphql.bff.type.Role
+import com.workshop.graphql.bff.type.User
+
 data class UserDto(
     val id: String,
     val username: String,
     val email: String?,
     val role: String,
     val groupId: String,
-)
+){
+    fun toGraphqlType(): User {
+        return User(
+            id = id,
+            username = username,
+            role = Role.valueOf(role),
+            email = email,
+            group = Group(groupId)
+        )
+    }
+}
