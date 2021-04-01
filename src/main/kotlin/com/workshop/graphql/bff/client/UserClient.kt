4cb3a4c02a09161e3575b444fe5cc794dbe31ea6
Zhang Gang <gzhang@thoughtworks.com>
2021-04-01 12:03:53 +0800

    add find all user client

--- src/main/kotlin/com/workshop/graphql/bff/client/UserClient.kt
+++ src/main/kotlin/com/workshop/graphql/bff/client/UserClient.kt
@@ -4,6 +4,7 @@ import com.workshop.graphql.bff.client.dto.UserDto
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @Component
@@ -26,5 +27,31 @@ class UserClient {
             Mono.error(IllegalStateException("not found"))
         }
 
+    }
+
+    fun findAll(): Flux<UserDto> {
+        return Flux.just(
+            UserDto(
+                id = "1",
+                username = "user1",
+                email = null,
+                role = "ADMIN",
+                groupId = "g1"
+            ),
+            UserDto(
+                id = "2",
+                username = "user2",
+                email = "user2@gmail.com",
+                role = "USER",
+                groupId = "g2"
+            ),
+            UserDto(
+                id = "3",
+                username = "user3",
+                email = "user3@gmail.com",
+                role = "USER",
+                groupId = "g3"
+            )
+        )
     }
 }
