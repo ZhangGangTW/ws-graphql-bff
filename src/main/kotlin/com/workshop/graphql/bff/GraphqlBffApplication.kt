7c7701d4d23fa0f273edc58373f42776a0efe8f6
Zhang Gang <gzhang@thoughtworks.com>
2021-04-06 00:56:31 +0800

    enable cors

--- src/main/kotlin/com/workshop/graphql/bff/GraphqlBffApplication.kt
+++ src/main/kotlin/com/workshop/graphql/bff/GraphqlBffApplication.kt
@@ -2,10 +2,31 @@ package com.workshop.graphql.bff
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsWebFilter
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
+import org.springframework.web.reactive.config.EnableWebFlux
+
 
 @SpringBootApplication
-class GraphqlBffApplication
+@EnableWebFlux
+class GraphqlBffApplication {
+    @Bean
+    fun corsWebFilter(): CorsWebFilter {
+        val corsConfig = CorsConfiguration()
+        corsConfig.allowedOrigins = listOf("*")
+        corsConfig.maxAge = 8000L
+        corsConfig.addAllowedMethod("POST")
+        corsConfig.addAllowedHeader("*")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", corsConfig)
+        return CorsWebFilter(source)
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<GraphqlBffApplication>(*args)
+    runApplication<GraphqlBffApplication>(*args)
 }
+
+
