76e051927b47c799f71a2fe3fbc0f59ac2aee9de
Zhang Gang <gzhang@thoughtworks.com>
2021-04-15 16:46:15 +0800

    add user-service dep

--- src/main/kotlin/com/workshop/graphql/bff/client/UserClient.kt
+++ src/main/kotlin/com/workshop/graphql/bff/client/UserClient.kt
@@ -4,29 +4,22 @@ import com.workshop.graphql.bff.client.dto.UserDto
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @Component
-class UserClient {
+class UserClient(
+    private val userWebClient: WebClient
+) {
     private val log: Logger = LoggerFactory.getLogger(UserClient::class.java)
 
     fun findById(id: String): Mono<UserDto> {
         log.info("### find user by id: $id")
-        return if (id == "1") {
-            Mono.just(
-                UserDto(
-                    id = id,
-                    username = "user1",
-                    email = null,
-                    role = "ADMIN",
-                    groupId = "g1"
-                )
-            )
-        } else {
-            Mono.error(IllegalStateException("not found"))
-        }
-
+        return userWebClient.get()
+            .uri("/{id}", id)
+            .retrieve()
+            .bodyToMono(UserDto::class.java)
     }
 
     fun findAll(): Flux<UserDto> {
---
+++ src/main/kotlin/com/workshop/graphql/bff/client/WebClientsConfig.kt
@@ -0,0 +1,23 @@
+package com.workshop.graphql.bff.client
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.WebClient
+
+@Configuration
+class WebClientsConfig {
+    @Value("\${adapter.api-key}")
+    private lateinit var apiKey: String
+
+
+    @Bean
+    fun userWebClient(
+        @Value("\${adapter.user-service-host}") userServiceHost: String
+    ): WebClient {
+        return WebClient.builder()
+            .baseUrl(userServiceHost)
+            .defaultHeader("api-key", apiKey)
+            .build()
+    }
+}
