1a6b44dda3ebb38a2a2353531fa53ac4fdc3d633
Zhang Gang <gzhang@thoughtworks.com>
2021-05-24 14:54:08 +0800

    add spring native bug have issue

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,5 @@ COPY build/libs/graphql-bff-*.jar /app/app.jar
 
 CMD ["java", "-jar", "/app/app.jar"]
 
-#docker build -t hataketed/ws-graphql-bff:3.0.0 .
-#docker push hataketed/ws-graphql-bff:3.0.0
+#docker build -t hataketed/ws-graphql-bff:4.0.0 .
+#docker push hataketed/ws-graphql-bff:4.0.0
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
 	id("org.springframework.boot") version "2.4.4"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.4.31"
-	kotlin("plugin.spring") version "1.4.31"
+	kotlin("jvm") version "1.4.32"
+	kotlin("plugin.spring") version "1.4.32"
+	id("org.springframework.experimental.aot") version "0.9.2"
 }
 
 group = "com.workshop"
-version = "0.0.1-SNAPSHOT"
+version = "4.0.1"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 configurations {
@@ -18,6 +20,7 @@ configurations {
 }
 
 repositories {
+	maven { url = uri("https://repo.spring.io/release") }
 	mavenCentral()
 }
 
@@ -30,10 +33,9 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	implementation("com.graphql-java:graphql-java:16.2")
-	implementation("com.expediagroup:graphql-kotlin-spring-server:4.0.0-alpha.17")
+	implementation("com.expediagroup:graphql-kotlin-spring-server:4.1.1")
 
 
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
@@ -48,4 +50,10 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.withType<BootBuildImage> {
+	builder = "paketobuildpacks/builder:tiny"
+	imageName = "hataketed/${rootProject.name}:${version}"
+	environment = mapOf("BP_NATIVE_IMAGE" to "true")
 }
